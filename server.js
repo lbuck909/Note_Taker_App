5609d78758e7c8e5feb59877c0dbe3db10b50925
LaiSha Buck <laishabuck@gmail.com>
2023-05-06 18:47:41 -0400

    needed correction to json PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const api_routes = require('./routes/api_routes');
 const html_routes = require('./routes/html_routes');
 
-// Initialize express and the PORT metod
+// Initialize express function and the PORT metod
 const app = express();
 const PORT = process.env.PORT || 3001;
 
