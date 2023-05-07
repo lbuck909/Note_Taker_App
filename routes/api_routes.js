7f6ec07a2fa5f1b38ccb4936b8b8c8f0fa6cc618
LaiSha Buck <laishabuck@gmail.com>
2023-05-06 21:36:25 -0400

    api routes updated

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,3 +1,40 @@
-const router = require('express').Router();
+const util = require('util');
 const fs = require('fs');
-const store = require('..db')
+//Node.js NPM uuid used to generate indiv ids
+const { v4: uuidv4 } = require('uuid');
+const { error } = require('console');
+
+// Use readFileAsync and writeFileAsync to generate actual notes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  read() {
+    return readFileAsync('db/db.json', 'utf-8');
+  }
+  write(note){
+    return writeFileAsync('db/db.json', JSON.stringify(note));
+  }
+  addNotes(note) {
+    const { title, text } = note
+
+    if ( !title || !text) {
+      throw new Error('title and text cannot be empty')
+    }
+
+    const userNote = { title, text, id: uuidv4() }
+
+    return this.newNote()
+    .then(notes => [...notes, newNote])
+    .then(currentNotes => this.write(currentNotes))
+    .then(() => this newNote)
+
+  }
+  newNote() {
+    return this.read()
+    .then(notes => {
+
+    })
+  }
+}
+
